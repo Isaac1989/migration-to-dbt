8fcd3df54c40725e8c074447c929a51f9f6c0d89
Isaac Oduro <senatoduro8@gmail.com>
2025-02-21 13:50:51 -0500

    Refactored a different legacy code

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -7,8 +7,10 @@ payments as (
   select
     id as payment_id,
     orderid as order_id,
+    created,
     status as payment_status,
     round(amount/100.0,2) as payment_amount
+   
 
   from source
 )
